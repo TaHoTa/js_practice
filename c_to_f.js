449714fc3a765719f3d3905a681da372e6fc944f
TaHoTa <tazammulhossain@gmail.com>
2022-01-31 10:05:43 +0600

    c to f

--- c_to_f.js
+++ c_to_f.js
@@ -1,3 +1,12 @@
 function c2f(celcious) {
-
+    let far = (celcious * (9 / 5) + 32);
+    let far2 = far.toFixed(2);
+    return far2;
 }
+
+
+let myCel = 33;
+console.log('my Fahrenheit is', c2f(myCel));
+
+let hisCel = 37;
+console.log('his Fahrenheit is', c2f(hisCel));
